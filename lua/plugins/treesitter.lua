7978483bacdb8437441caaad483246def4cef92a
Psiridis <psiridis.konstantinos@gmail.com>
2023-12-17 18:51:25 +0200

    New directory structure for plugins. Lazy will automatically load everything if there is the following directory ./lua/plugins as long as it returns a Lua module /Lua tables

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+        ensure_installed = {
+          "lua",
+          "javascript",
+          "json",
+          "java",
+          "css",
+          "vim",
+          "c",
+          "cpp",
+          "python",
+        },
+        highlight = { enable = true },
+        indent = { enable = true },  
+      })
+    end
+  }
+}
